7ebf3adeb54325ec32cf92d7252224ea2ce1dade
Catherine Cherubin <cat.cherubin@gmail.com>
2019-04-10 13:18:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+  var katzDeliLine = [];
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + line.length + " in line."
+  
+}
